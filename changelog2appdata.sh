8fe5f3c32c766723a27b4584eb43c26a86e0adfc
Alexander Wilms <f.alexander.wilms@gmail.com>
2021-02-15 19:27:34 +0100

    Make changelog conversion more robust

--- changelog2appdata.sh
+++ changelog2appdata.sh
@@ -5,7 +5,7 @@ RELEASE_OPENING_TAG="<release date=\"$RELEASE_DATE\" version=\"$TXS_VERSION\">"
 DESCRIPTION_OPENING_TAG="<description>"
 CHANGES="<p>Changes:</p>"
 LIST_OPENING_TAG="<ul>"
-CHANGELOG=$(head utilities/manual/CHANGELOG.txt| tail -n +3 | sed -e 's|- |<li>|g' | sed -e 's|$|</li>|g')
+CHANGELOG=$(sed -e '/./!Q' utilities/manual/CHANGELOG.txt | tail -n +3 | sed -e 's|-\ |<li>|g' | sed -e 's|$|</li>|g')
 CHANGELOG="${CHANGELOG//
 /\\n}"
 LIST_CLOSING_TAG="</ul>"
@@ -13,7 +13,7 @@ DESCRIPTION_CLOSING_TAG="</description>"
 RELEASE_CLOSING_TAG="</release>"
 
 sed -i '/releases/,/releases/{//!d}' utilities/texstudio.appdata.xml
-sed -e "s|<releases>|<releases>\n$RELEASE_OPENING_TAG\n$DESCRIPTION_OPENING_TAG\n$CHANGES\n$LIST_OPENING_TAG\n$CHANGELOG\n$LIST_CLOSING_TAG\n$DESCRIPTION_CLOSING_TAG\n$RELEASE_CLOSING_TAG|g" -i utilities/texstudio.appdata.xml
+sed -e "s|<releases>|<releases>$RELEASE_OPENING_TAG$DESCRIPTION_OPENING_TAG$CHANGES$LIST_OPENING_TAG$CHANGELOG$LIST_CLOSING_TAG$DESCRIPTION_CLOSING_TAG$RELEASE_CLOSING_TAG|g" -i utilities/texstudio.appdata.xml
 
 cp utilities/texstudio.appdata.xml utilities/texstudio.appdata.xml.bak
 xmllint --format utilities/texstudio.appdata.xml.bak > utilities/texstudio.appdata.xml
