cdfa4d162d1a99dc0916e44d5b92b3a583324589
Alexander Wilms <Alexander-Wilms@users.noreply.github.com>
2022-10-31 03:13:44 +0100

    Update texstudio.sh

--- texstudio.sh
+++ texstudio.sh
@@ -8,4 +8,4 @@ export PATH=/usr/bin:/app/bin:/app/texlive/bin:/app/texlive/bin/x86_64-linux:/ap
 export PERL5LIB=/app/texlive/lib/perl5/site_perl/$perl_version/:/app/texlive/lib/perl5/$perl_version/
 # add library paths
 export LD_LIBRARY_PATH=/app/texlive/lib/:/app/texlive/lib/perl5/$perl_version/x86_64-linux/CORE/:/app/texlive/lib/perl5/$perl_version/aarch64-linux/CORE/
-texstudio $1
+texstudio "$@"
