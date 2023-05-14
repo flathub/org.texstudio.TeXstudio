85b88a709bfeacda8f1e4a081f064427281b8641
Alexander Wilms <Alexander-Wilms@users.noreply.github.com>
2023-05-14 15:12:30 +0200

    Use changelog2metainfo.sh

---
+++ changelog2metainfo.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+cd .github
+. scripts/get-version.sh > /dev/null
+cd ..
+RELEASE_OPENING_TAG="<release date=\"$RELEASE_DATE\" version=\"$TXS_VERSION\">"
+DESCRIPTION_OPENING_TAG="<description>"
+CHANGELOG=$(sed '/^[[:space:]]*$/d' utilities/manual/source/CHANGELOG.md | tail -n +2 | awk 'NR==2,/##/' | head -n -1 | pandoc --from markdown --to html5 )
+DESCRIPTION_CLOSING_TAG="</description>"
+RELEASE_CLOSING_TAG="</release>"
+
+sed -i '/releases/,/releases/{//!d}' utilities/texstudio.metainfo.xml
+
+if [ "$CHANGELOG" != "<li></li>" ]; then
+	RELEASE_TAG=$RELEASE_OPENING_TAG$DESCRIPTION_OPENING_TAG$CHANGELOG$DESCRIPTION_CLOSING_TAG$RELEASE_CLOSING_TAG
+else
+	RELEASE_TAG=$RELEASE_OPENING_TAG$RELEASE_CLOSING_TAG
+fi
+
+# this avoids an issue with sed
+# https://unix.stackexchange.com/a/360541/148421
+cat utilities/texstudio.metainfo.xml | xmllint --format - | awk 'NR==1,/<releases>/' | echo "$(</dev/stdin)$RELEASE_TAG</releases></component>" > utilities/texstudio.metainfo.xml.new
+rm utilities/texstudio.metainfo.xml
+xmllint --format utilities/texstudio.metainfo.xml.new > utilities/texstudio.metainfo.xml && \
+rm utilities/texstudio.metainfo.xml.new
