5d554ba750352e5407710a9088e8d02ac899500d
Alexander Wilms <Alexander-Wilms@users.noreply.github.com>
2023-08-03 00:11:57 +0200

    Try fixing TeXlive

--- texstudio.sh
+++ texstudio.sh
@@ -7,10 +7,10 @@
      # add paths of TeXlive Flatpak extension binaries
      export PATH=/usr/bin:/app/bin:/app/texlive/bin:/app/texlive/bin/x86_64-linux:/app/texlive/bin/aarch64-linux
      # add include paths for Perl @INC variable
-     export PERL5LIB=/app/texlive/lib/perl5/site_perl/$perl_version/:/app/texlive/lib/perl5/$perl_version/
+     export PERL5LIB=/app/texlive/lib/perl5/$perl_version:/app/texlive/lib/perl5/site_perl/$perl_version
      # add library paths
-     export LD_LIBRARY_PATH=/app/texlive/lib/:/app/texlive/lib/perl5/$perl_version/x86_64-linux/CORE/:/app/texlive/lib/perl5/$perl_version/aarch64-linux/CORE/
- fi
+     export LD_LIBRARY_PATH=/app/texlive/lib:/app/texlive/lib/perl5/$perl_version/x86_64-linux/CORE
+fi
  
 # taken from
 # https://dev.languagetool.org/http-server
