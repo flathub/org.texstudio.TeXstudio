d21b95d88c03c2a92c7f3f5e37acb01d94f7c265
Sebastian Grabowski <sebastian@grabel.de>
2023-08-19 22:46:25 +0200

    Add extension point for ngram data
    LanguageTool needs extra ngram data files. This adds an extension
    point so that we can optionally install these as a flatpak extension.

--- texstudio.sh
+++ texstudio.sh
@@ -18,4 +18,4 @@ fi
 # https://github.com/languagetool-org/languagetool
 # make sure to kill java so we don't end up with lots of unused LanguageTool server instances
 # https://stackoverflow.com/questions/6674327/redirect-all-output-to-file-in-bash
-/app/jre/bin/java -cp /app/languagetool/languagetool-server.jar org.languagetool.server.HTTPServer --port 8081 --allow-origin > /dev/null 2>&1 & texstudio "$@" && pkill -SIGKILL java
+/app/jre/bin/java -cp /app/languagetool/languagetool-server.jar org.languagetool.server.HTTPServer --port 8081 --allow-origin --languageModel /app/ngrams > /dev/null 2>&1 & texstudio "$@" && pkill -SIGKILL java
