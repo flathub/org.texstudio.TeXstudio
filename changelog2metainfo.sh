846615d6ccfcb52a13b0ce7645ddfad3cd72ed96
Alexander Wilms <Alexander-Wilms@users.noreply.github.com>
2024-05-12 09:45:28 +0200

    changelog2metainfo: Remove ** from Markdown

--- changelog2metainfo.sh
+++ changelog2metainfo.sh
@@ -8,6 +8,8 @@ DESCRIPTION_OPENING_TAG="<description>"
 # turn links into plain text
 # [#3458](https://github.com/texstudio-org/texstudio/pull/3458) -> #3458
 sed -i -E 's|\[([^]]*)]\(([^)]*)\)|\1|g' utilities/manual/source/CHANGELOG.md
+# pandoc turns ** into <strong/>, which is not supported
+sed -i 's|**||g' utilities/manual/source/CHANGELOG.md
 CHANGELOG=$(sed '/^[[:space:]]*$/d' utilities/manual/source/CHANGELOG.md | tail -n +2 | awk 'NR==2,/##/' | head -n -1 | pandoc --from markdown --to html5 )
 DESCRIPTION_CLOSING_TAG="</description>"
 RELEASE_CLOSING_TAG="</release>"
