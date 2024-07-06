f4a3409890ef92753bf220fcddc0affd4fb19f6e
Alexander Wilms <Alexander-Wilms@users.noreply.github.com>
2024-07-06 02:18:18 +0200

    Don't remove flatpak-wrapper and flatpak-spawn from texstudio.ini any longer

--- texstudio.sh
+++ texstudio.sh
@@ -10,12 +10,6 @@ if [ -d $perl_dir ]; then
     export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/app/texlive/lib/perl5/$perl_version/x86_64-linux/CORE
 fi
 
-# flatpak-wrapper is deprecated, TeXstudio itself now prepends `flatpak-spawn --host` if necessary
-# But since the TeXstudio settings of existing installations still point to flatpak-wrapper
-# we need to remove it from the .ini file
-sed -i 's|flatpak-wrapper ||g' ~/.var/app/org.texstudio.TeXstudio/config/texstudio/texstudio.ini
-sed -i 's|flatpak-spawn --host ||g' ~/.var/app/org.texstudio.TeXstudio/config/texstudio/texstudio.ini
-
 # taken from
 # https://dev.languagetool.org/http-server
 # linked here:
